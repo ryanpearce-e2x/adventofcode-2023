e77a8b941e94f98f60041e0c4746142b9bedafa4
Ryan Pearce <ryan.pearce@applydigital.com>
2023-12-20 16:20:45 +0000

    day 4 part 2

--- src/days/4.ts
+++ src/days/4.ts
@@ -1,6 +1,7 @@
 import { Day, IDay, Solution } from '../types/Day';
 
 interface Card {
+    id: number;
     winningNumbers: number[];
     numbersWeHave: number[];
 }
@@ -12,17 +13,23 @@ export default class Day4 extends Day implements IDay {
 
     private getCards(): Card[] {
         const lines = this.input.split('\n');
-        return lines.map((line) => {
+        return lines.map((line, i) => {
+            const id = i + 1;
             const [, numbersString] = line.split(':');
             const [winningNumbersString, numbersWeHaveString] = numbersString.split('|');
             const winningNumbers = winningNumbersString.trim().split(/ {1,}/).map(Number);
             const numbersWeHave = numbersWeHaveString.trim().split(/ {1,}/).map(Number);
-            return { winningNumbers, numbersWeHave };
+            return { id, winningNumbers, numbersWeHave };
         });
     }
 
-    private getCardPointValue(card: Card): number {
+    private getWinningNumbers(card: Card): number[] {
         const winningNumbersWeHave = card.numbersWeHave.filter((x) => card.winningNumbers.includes(x));
+        return winningNumbersWeHave;
+    }
+
+    private getCardPointValue(card: Card): number {
+        const winningNumbersWeHave = this.getWinningNumbers(card);
         return winningNumbersWeHave.length ? Math.pow(2, winningNumbersWeHave.length - 1) : 0;
     }
 
@@ -33,6 +40,17 @@ export default class Day4 extends Day implements IDay {
     }
 
     partTwo(): Solution {
-        return 0;
+        const cards = this.getCards();
+        const cardCopies: Record<number, number> = {};
+        cards.forEach((card) => (cardCopies[card.id] = 1));
+        cards.forEach((card) => {
+            const winningNumbers = this.getWinningNumbers(card);
+            const maxId = card.id + winningNumbers.length;
+            for (let i = card.id + 1; i <= maxId; i++) {
+                cardCopies[i] = (cardCopies[i] || 0) + cardCopies[card.id];
+            }
+        });
+        const totalCopies = Object.values(cardCopies).reduce((acc, curr) => acc + curr, 0);
+        return totalCopies;
     }
 }
