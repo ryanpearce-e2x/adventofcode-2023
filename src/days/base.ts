9efe259bd993b5d903605570c0cd36f48b392db0
Ryan Pearce <ryan.pearce@applydigital.com>
2023-12-21 11:25:44 +0000

    update base constructor

--- src/days/base.ts
+++ src/days/base.ts
@@ -1,8 +1,8 @@
 import { Day, IDay, Solution } from '../types/Day';
 
 export default class DayX extends Day implements IDay {
-    constructor() {
-        super(0);
+    constructor(skip: boolean = false) {
+        super(0, skip);
     }
 
     partOne(): Solution {
