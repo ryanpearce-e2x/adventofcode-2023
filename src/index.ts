ec32183ce622e0820c10010a42cb2eb3ca59c739
Ryan Pearce <ryan.pearce@applydigital.com>
2023-12-27 09:12:28 +0000

    allow specific day as argument

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Day1, Day10, Day11, Day12, Day13, Day14, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9 } from './days';
+import { IDay } from './types/Day';
 
 const measureExecutionTime = (callback: () => void): number => {
     const startTime = performance.now();
@@ -13,7 +14,7 @@ const currentDays = [
     new Day2(),
     new Day3(),
     new Day4(),
-    new Day5(true),
+    new Day5(),
     new Day6(),
     new Day7(),
     new Day8(),
@@ -25,19 +26,24 @@ const currentDays = [
     new Day14(),
 ];
 
-currentDays.forEach((day, i) => {
-    console.log(`-----Day ${i + 1}-----`);
-    if (!day.skip) {
-        const p1Time = measureExecutionTime(() => {
-            console.log('Part 1:', day.partOne());
-        });
-        console.log('Part 1 executed in', p1Time, 'ms');
-        const p2Time = measureExecutionTime(() => {
-            console.log('Part 2:', day.partTwo());
-        });
-        console.log('Part 2 executed in', p2Time, 'ms');
-    } else {
-        console.log('SKIPPED');
-    }
+const runDay = (day: IDay, dayNumber: number) => {
+    console.log(`-----Day ${dayNumber}-----`);
+    const p1Time = measureExecutionTime(() => {
+        console.log('Part 1:', day.partOne());
+    });
+    console.log('Part 1 executed in', p1Time, 'ms');
+    const p2Time = measureExecutionTime(() => {
+        console.log('Part 2:', day.partTwo());
+    });
+    console.log('Part 2 executed in', p2Time, 'ms');
     console.log('---------------');
-});
+};
+
+const dayArg = parseInt(process.argv[2]);
+if (!isNaN(dayArg) && dayArg >= 1 && dayArg <= currentDays.length) {
+    runDay(currentDays[dayArg - 1], dayArg);
+} else {
+    currentDays.forEach((day, i) => {
+        runDay(day, i + 1);
+    });
+}
